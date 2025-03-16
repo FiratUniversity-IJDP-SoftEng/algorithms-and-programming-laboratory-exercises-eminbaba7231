38bed2636a8f91d5ee2621d67fc8afe619d077f0
Emin Talha Çelik <86913426+eminbaba7231@users.noreply.github.com>
2025-03-16 13:11:07 +0300

    Update ex002.py

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543008
+# Your Name and Surname:Emin Talha Celik
+import sys
+print("Python version:", sys.version)
+print("Version info:", sys.version_info)

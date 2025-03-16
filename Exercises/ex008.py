56f7e7fe1b8a1131d4e8e9afafe482544158ec9d
Emin Talha Çelik <86913426+eminbaba7231@users.noreply.github.com>
2025-03-16 13:16:16 +0300

    Update ex008.py

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,7 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 230543008
+# Your Name and Surname:Emin Talha Celik
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    spaces = ' ' * (n - i)
+    stars = '*' * (2 * i - 1)
+    print(spaces + stars)
